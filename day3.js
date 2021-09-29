500879266f422abbc50036857147b84edee21fa6
Chandana25_Sudo <chandana.velpula16@gmail.com>
2021-09-29 16:26:19 +0530

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,6 +1,26 @@
-let numbers = [1,2,3,4,6,7,8,9];
-console.log(numbers);
+
 let buttonOperation = () => {
-    numbers.push(9,10,11,12,13);
-    console.log(numbers);
+    let numbers = [1,2,3,4,6,7,8,9];
+    console.log("numbers before adding-"+numbers);
+    numbers.push(10,11,12,13,14);
+    console.log("numbers after adding-" +numbers);
+    // for(let i=0;i<numbers.length;i++) {
+    //   numbers[i] = numbers[i]*10;
+    // }
+    // console.log("numbers with multiple of 10-" +numbers);
+    let multipiedArray = numbers.map(x => {
+        return x*10;
+    })
+     console.log("numbers with multiple of 10-" +multipiedArray);
+    //  let even;
+    let even=[];
+    numbers.map(x => {
+        
+        if(x%2==0) {
+           
+           even.push(x);
+        }
+       
+    })
+    console.log("even numbers in given list are-" +even);
 }
